a00d493c176d0a2403b4353a04860e14666f146c
najmul-islam <najmulislam519@gmail.com>
2022-12-12 21:20:08 +0600

    start project

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Home from "../components/Home";
+import Member from "../components/Member";
+
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+      <Member />
+    </>
+  );
+};
+
+export default HomePage;
